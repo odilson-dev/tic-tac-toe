049be8dc2ce45d051aca6a99e5fa4f44ffab6825
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-22 11:44:52 -0400

    Implement check_victory method
    The check_victory method returns true if someone wins

--- main.rb
+++ main.rb
@@ -1,23 +1,48 @@
+
+def verify(elem1, elem2, elem3)
+    elem1 == elem2 ? elem2 == elem3 : false
+end
+
 class Board
-  def initialize
-    @board = {
-        1 => 1, 2 => 2, 3 => 3,
-        4 => 4, 5 => 5, 6 => 6,
-        7 => 7, 8 => 8, 9 => 9
-        }
+  
+  @@board = {
+    1 => 1, 2 => 2, 3 => 3,
+    4 => 4, 5 => 5, 6 => 6,
+    7 => 7, 8 => 8, 9 => 9
+    }
+
+  def self.show_board
+      puts "      #{@@board[1]} | #{@@board[2]} | #{@@board[3]}"
+      puts '     ---+---+---'
+      puts "      #{@@board[4]} | #{@@board[5]} | #{@@board[6]}"
+      puts '     ---+---+---'
+      puts "      #{@@board[7]} | #{@@board[8]} | #{@@board[9]}"
   end
 
-  def show_board
-      puts "      #{@board[1]} | #{@board[2]} | #{@board[3]}"
-      puts '     ---+---+---'
-      puts "      #{@board[4]} | #{@board[5]} | #{@board[6]}"
-      puts '     ---+---+---'
-      puts "      #{@board[7]} | #{@board[8]} | #{@board[9]}"
+  def self.check_victory
+
+    arr = [ #Vertical
+            verify(@@board[1], @@board[2], @@board[3]),
+            verify(@@board[4], @@board[5], @@board[6]),
+            verify(@@board[7], @@board[8], @@board[9]),
+
+            #Horizontal
+            verify(@@board[1], @@board[4], @@board[7]),
+            verify(@@board[2], @@board[5], @@board[8]),
+            verify(@@board[3], @@board[6], @@board[9]),
+
+            #Diagonal
+            verify(@@board[1], @@board[5], @@board[9]),
+            verify(@@board[7], @@board[5], @@board[3]),
+    ]
+    arr.any?
   end
 end
+p Board.check_victory
+
 class Player
     def initialize(name, player_symbol)
-        @name = name
-        @player_symbol = player_symbol
+        name = name
+        player_symbol = player_symbol
     end
 end
